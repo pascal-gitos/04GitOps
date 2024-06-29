a56d4bffbd9bb0eff88d97d4ed1ea5535e3e5e37
Nikita <vedro.for.emails@gmail.com>
2024-06-29 18:56:59 +0300

    add string operations & test & github action

---
+++ README.md
@@ -0,0 +1,12 @@
+# GitOps
+1.new rep 
+```bash
+https://github.com/pascal-gitos/04GitOps
+```
+
+2.code in `hello.py`
+ 
+```python
+print("hello")
+```
+
---
+++ stringOperationTest.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestConcatenate(t *testing.T) {
+	result := Concatenate("String ", "concatenate")
+	expected := "String concatenate"
+	if result != expected {
+		t.Error("Concatenation test failed. Expected: %s, got: %s", expected, result)
+
+	}
+}
+
+func TestUppercase(t *testing.T) {
+	result := Uppercase("string uppercase")
+	expected := "STRING UPPERCASE"
+	if result != expected {
+		t.Error("Upppercase test failed. Expected: %s, got %s", expected, result)
+	}
+}
